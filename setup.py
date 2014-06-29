66b47c057cf47d54497257a0be3d0504e3b43129
Takahiro Ikeuchi <takahiro_ikeuchi@albert2005.co.jp>
2014-06-29 22:46:36 +0900

    - Created repository - Implemented minimum methods
    refs #1

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+__author__ = 'Takahiro Ikeuchi'
+
+setup(
+    name = "hipchatpy",
+    version = "0.0.1",
+    packages=['hipchatpy'],
+    long_description=open('README.md').read(),
+)
+
