005d37e5f121afb7a64afe045c06290c49e6defe
Takahiro Ikeuchi <takahiro_ikeuchi@albert2005.co.jp>
2014-06-30 00:13:48 +0900

    Modified setup.py for release 0.0.3

--- setup.py
+++ setup.py
@@ -6,14 +6,13 @@ __author__ = 'Takahiro Ikeuchi'
 
 setup(
     name="hipchatpy",
-    version="0.0.2",
+    version="0.0.3",
     packages=['hipchatpy'],
     install_requires=open('requirements.txt').read().splitlines(),
     description="Simple HipChat client library",
     long_description=open('README.txt').read(),
     author='Takahiro Ikeuchi',
     author_email='takahiro.ikeuchi@gmail.com',
-    author_twitter='@iktakahiro',
     url='https://github.com/iktakahiro/hipchatpy',
     keywords="HipChat client",
 )
