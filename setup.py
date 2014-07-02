d3e97f6b39f5470ceb12ea87e5779f5604ff87eb
Takahiro Ikeuchi <takahiro_ikeuchi@albert2005.co.jp>
2014-07-02 19:53:09 +0900

    Removed constant
    refs #5

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ __author__ = 'Takahiro Ikeuchi'
 
 setup(
     name="hipchatpy",
-    version="0.0.4",
+    version="0.0.5",
     packages=['hipchatpy'],
     install_requires=open('requirements.txt').read().splitlines(),
     description="Simple HipChat client library",
