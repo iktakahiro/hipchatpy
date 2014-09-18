7717c812da8eba8d42d3c8dcc1c7982732c7c5fe
Takahiro Ikeuchi <takahiro_ikeuchi@albert2005.co.jp>
2014-09-19 01:00:36 +0900

    Changed setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ __author__ = 'Takahiro Ikeuchi'
 
 setup(
     name="hipchatpy",
-    version="0.1.15",
+    version="0.1.16",
     py_modules=['hipchatpy'],
     package_dir={'': 'hipchatpy'},
     install_requires=open('requirements.txt').read().splitlines(),
